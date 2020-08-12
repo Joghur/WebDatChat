41587a2a6079788cf1888ea33db3c5c83a7469f7
Adnan-Bacic <adnan-bacic@hotmail.com>
2020-08-12 16:06:08 +0200

    Update UserListScreen.js

--- screens/UserListScreen.js
+++ screens/UserListScreen.js
@@ -1,12 +1,59 @@
-import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, Text, StyleSheet, Button, ScrollView, TouchableWithoutFeedback } from "react-native";
 
 const UserListScreen = (props) => {
+
+  const [usersArray, setUsersArray] = useState([])
+
+  const onUserClick = () => {
+    console.log('user clicked')
+  }
+
+  useEffect(() => {
+    try{
+        const fetchData = async () => {
+        const response = await fetch("https://firestore.googleapis.com/v1beta1/projects/webdatchat-772f0/databases/(default)/documents/users/")
+        const data = await response.json()
+
+        //we have to map through an array. data is an object while data.documents is an array
+        let dataArray = data.documents
+
+       let mappedArray = dataArray.map((item, index) => {
+
+         //specifc name and email data
+         /*
+         console.log(item.fields.name.stringValue)
+         console.log(item.fields.email.stringValue)
+         */
+
+         //return the content
+        return (
+          <TouchableWithoutFeedback onPress={onUserClick} key={index}>
+            <View style={styles.usersContainer}>
+              <Text>{item.fields.name.stringValue}</Text>
+              <Text>{item.fields.email.stringValue}</Text>
+            </View>
+          </TouchableWithoutFeedback>
+        )
+       })
+       //set the array in the state
+       setUsersArray([mappedArray])
+
+        return data
+      }
+
+      fetchData()
+    }
+    catch(err){
+      console.log(err)
+    }
+  }, [])
+  
+
   return (
-    <View>
-      <Text> Userlist Screen</Text>
-      <Button title="Go to Home" onPress={() => props.navigation.popToTop()} />
-    </View>
+    <ScrollView>
+        {usersArray}
+    </ScrollView>
   );
 };
 
@@ -14,6 +61,11 @@ export const screenOptions = {
   headerTitle: "Userlist Screen",
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  usersContainer:{
+    flex: 1,
+    marginBottom: 50
+  }
+});
 
 export default UserListScreen;
